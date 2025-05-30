5922b98698a998bf18fa376cd95cdb7db8accdee
Faisal-akra <158831339+Faisal-akra@users.noreply.github.com>
2025-05-31 00:36:59 +0500

    add dashboard structure

--- src/custom-components/Dashboard.jsx
+++ src/custom-components/Dashboard.jsx
@@ -1,20 +1,35 @@
-
-
-
 function Dashboard() {
-
-  const remove = () => {
-    localStorage.removeItem('token')
-  }
-  return(
-    <div>
-        <div>
-          <div>Completed</div>
-          <div><button onClick={remove}>Click</button></div>
-          <div></div>
+  return (
+    <div className="">
+      <div className=" m-5 ">
+        <div className="mb-8">
+          <h1 className="font-extrabold text-xl">Task-Management</h1>
         </div>
-    </div>
-  )
-}
 
+        <div className=" flex justify-between gap-4 ">
+          <div className="gap-3 flex flex-col font-bold w-[200px] ">
+            <div className="text-center mb-4">
+              <p className=" font-bold  ">Name</p>
+              <p>zjan45097@gmail.com</p>
+            </div>
+
+            <p>Craete-Task</p>
+            <p>Dashboard</p>
+            <p>Tasks</p>
+            <p>Pending Tasks</p>
+            <p>Completed Tasks</p>
+            <p>To-Do Tasks</p>
+            <p>Logout</p>
+          </div>
+
+          <div className="flex justify-evenly  w-[800px]">
+            <p>Completed</p>
+            <p>Pending</p>
+            <p>To-Do</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default Dashboard;
