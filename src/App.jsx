794352d38cdf0446a9a933d2e5a7e15bb011b2e1
Faisal-akra <158831339+Faisal-akra@users.noreply.github.com>
2025-05-30 22:25:30 +0500

    routing setup protected route

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,41 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./custom-components/LoginForm";
 import Dashboard from "./custom-components/Dashboard";
 import ProtectedRoute from "./custom-components/ProtectedRoute";
-
+import AuthRedirect from "./custom-components/AuthRedirect";
 
 function App() {
   return (
     <div>
-     <Routes>
-      <Route path="/" element={<Register/>}/>
-      <Route path="/login" element={<ProtectedRoute>
-        <Login/>
-      </ProtectedRoute>} />
-      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <AuthRedirect>
+              <Register />
+            </AuthRedirect>
+          }
+        />
+
+        <Route
+          path="/login"
+          element={
+            <AuthRedirect>
+              <Login />
+            </AuthRedirect>
+          }
+        />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/custom-components/Dashboard.jsx
+++ src/custom-components/Dashboard.jsx
@@ -2,11 +2,15 @@
 
 
 function Dashboard() {
+
+  const remove = () => {
+    localStorage.removeItem('token')
+  }
   return(
     <div>
         <div>
           <div>Completed</div>
-          <div></div>
+          <div><button onClick={remove}>Click</button></div>
           <div></div>
         </div>
     </div>
